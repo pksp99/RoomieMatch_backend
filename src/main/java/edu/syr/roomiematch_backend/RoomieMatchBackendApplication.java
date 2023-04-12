3faebc7d43ec8492439ad85b5cf71463224af19a
Preet Karia <karia.preet5@gmail.com>
2023-04-12 18:09:43 -0400

    Initial commit

---
+++ src/main/java/edu/syr/roomiematch_backend/RoomieMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.syr.roomiematch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomieMatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomieMatchBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/syr/roomiematch_backend/RoomieMatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.syr.roomiematch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieMatchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
